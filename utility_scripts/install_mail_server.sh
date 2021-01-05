3522027e3ff0139b8f89d89658c8e3d090a91c61
packetalien <packetalien@packetalien.com>
2021-01-04 23:11:16 -0600

    Updated installer with cert config for dovecot

--- utility_scripts/install_mail_server.sh
+++ utility_scripts/install_mail_server.sh
@@ -9,6 +9,8 @@
 # Installing postfix
 echo "installing postfix..."
 sudo dnf -y install postfix
+echo "installing dovecot (IMAP(s)/POP3(s)"
+sudo dnf -y install dovecot
 echo "starting postfix..."
 sudo systemctl start postfix
 echo "enabling postfix..."
@@ -17,23 +19,20 @@ sudo systemctl enable postfix
 echo "Configuring /etc/postfix/main.cf for Demo in a Box..."
 
 sudo sed -i 's/#myhostname = host.domain.tld/myhostname = mail.demoinabox.net/g' /etc/postfix/main.cf
-
 sudo sed -i 's/#myorigin = $myhostname/myorigin = $myhostname/g' /etc/postfix/main.cf
-
 sudo sed -i 's/inet_interfaces = localhost/#inet_interfaces = localhost/g' /etc/postfix/main.cf
-
 sudo sed -i 's/#inet_interfaces = $myhostname/inet_interfaces = $myhostname/g' /etc/postfix/main.cf
-
 sudo sed -i -e '$a192.168.35.134 mail.demoinabox.net' /etc/hosts
-
-echo "installing dovecot (IMAP(s)/POP3(s)"
-sudo dnf -y install dovecot
 
 echo "configuring dovecot..."
 sudo sed -i -e '30alisten = 192.168.35.134, ::' /etc/dovecot/dovecot.conf
 
-sudo sed -i 's/ssl_cert = </etc/pki/dovecot/certs/dovecot.pem/ssl_cert = </etc/pki/tls/certs/diab_wildcard.crt/g' /etc/dovecot/conf.d/10-ssl.conf
-sudo sed -i 's/ssl_key = </etc/pki/dovecot/private/dovecot.pem/ssl_cert = </etc/pki/tls/private/diab_wildcard.key/g' /etc/dovecot/conf.d/10-ssl.conf
+echo "setting up dovecot keys and certs"
+sudo rm -rf /etc/pki/dovecot/certs/dovecot.pem
+sudo rm -rf /etc/pki/dovecot/private/dovecot.pem
+sudo cp /etc/pki/tls/private/diab_wildcard.key /etc/pki/dovecot/private/dovecot.pem
+sudo cp /etc/pki/tls/certs/diab_wildcard.crt /etc/pki/dovecot/certs/dovecot.pem
+
 
 echo "starting and enabling dovecot"
 sudo systemctl start dovecot
