004cd5095208a7c1c2f255f689969bccf7fa72de
packetalien <packetalien@packetalien.com>
2021-01-06 16:33:24 -0600

    Troubleshooting installer

--- utility_scripts/install_mail_server.sh
+++ utility_scripts/install_mail_server.sh
@@ -18,7 +18,6 @@ sudo systemctl enable postfix
 echo "Configuring /etc/postfix/main.cf for Demo in a Box..."
 
 sudo sed -i 's/#myhostname = host.domain.tld/myhostname = mail.demoinabox.net/g' /etc/postfix/main.cf
-#mydomain = domain.tld
 sudo sed -i 's/#mydomain = domain.tld/mydomain = demoinabox.net/g' /etc/postfix/main.cf
 sudo sed -i 's/#myorigin = $myhostname/myorigin = $myhostname/g' /etc/postfix/main.cf
 sudo sed -i 's/inet_interfaces = localhost/#inet_interfaces = localhost/g' /etc/postfix/main.cf
@@ -28,20 +27,23 @@ echo "Copying over built in keys as a backup."
 sudo cp /etc/pki/tls/private/diab_wildcard.key /etc/pki/tls/private/postfix.key
 sudo cp /etc/pki/tls/certs/diab_wildcard.crt /etc/pki/tls/certs/postfix.pem
 sudo sed -i -e '$asmtpd_use_tls=yes' /etc/postfix/main.cf
-sudo postconf -e "home_mailbox = Maildir/"
+sudo cp /home/panse/certs/'Demo in a Box CA.crt' /etc/ssl/certs/DiaBCA.crt
+sudo cp /home/panse/certs/'Demo in a Box CA.crt' /etc/pki/ca-trust/source/anchors/DiaBCA.crt
+sudo update-ca-trust extract
 
 echo "configuring dovecot..."
 sudo sed -i -e '24aprotocols = imap pop3' /etc/dovecot/dovecot.conf
 sudo sed -i -e '31alisten = 192.168.35.134, ::' /etc/dovecot/dovecot.conf
 #sudo sed -i 's/#mail_location =/mail_location = maildir\:\~\/Maildir' /etc/dovecot/conf.d/10-mail.conf
-sudo sed -i '30amail_location = maildir:~/Maildir' /etc/dovecot/conf.d/10-mail.conf
+sudo sed -i '30amail_location = mbox:~/:INBOX=/var/spool/mail/%u' /etc/dovecot/conf.d/10-mail.conf
 sudo sed -i '116amail_privileged_group = mail' /etc/dovecot/conf.d/10-mail.conf
 sudo gpasswd -a dovecot mail
-sudo sed -i 's/#disable_plaintext_auth = yes/disable_plaintext_auth = yes/g' /etc/dovecot/conf.d/10-auth.conf
+sudo sed -i 's/#disable_plaintext_auth = yes/disable_plaintext_auth = no/g' /etc/dovecot/conf.d/10-auth.conf
 sudo sed -i '55assl_dh = </etc/dovecot/dh.pem' /etc/dovecot/conf.d/10-ssl.conf
 sudo sed -i 's/#ssl_min_protocol = TLSv1/ssl_min_protocol = TLSv1.2/g' /etc/dovecot/conf.d/10-ssl.conf
 sudo sed -i 's/#ssl_prefer_server_ciphers = no/ssl_prefer_server_ciphers = yes/g' /etc/dovecot/conf.d/10-ssl.conf
-
+sudo sed -i '26assl_ca = /etc/ssl/certs/DiaBCA.crt' /etc/dovecot/conf.d/10-ssl.conf
+sudo sed -i 's/auth_mechanisms = plain/auth_mechanisms = plain login/g' /etc/dovecot/conf.d/10-auth.conf
 
 echo "setting up dovecot keys and certs"
 sudo rm -rf /etc/pki/dovecot/certs/dovecot.pem
@@ -60,7 +62,7 @@ sudo sed -i '19a\ \ -o smtpd_tls_security_level=encrypt' /etc/postfix/master.cf
 sudo sed -i '20a\ \ -o smtpd_tls_wrappermode=no' /etc/postfix/master.cf
 sudo sed -i '20a\ \ -o smtpd_sasl_auth_enable=yes' /etc/postfix/master.cf
 sudo sed -i '22a\ \ -o smtpd_relay_restrictions=permit_sasl_authenticated,reject' /etc/postfix/master.cf
-sudo sed -i '23a\ \ -o smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,reject' /etc/postfix/master.cf
+sudo sed -i '23a\ \ -o smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,reject_unauth_destination' /etc/postfix/master.cf
 sudo sed -i '24a\ \ -o smtpd_sasl_type=dovecot' /etc/postfix/master.cf
 sudo sed -i '25a\ \ -o smtpd_sasl_path=private/auth' /etc/postfix/master.cf
 
@@ -91,15 +93,6 @@ sudo sed -i '102auser = postfix' /etc/dovecot/conf.d/10-master.conf
 sudo sed -i '103agroup = postfix' /etc/dovecot/conf.d/10-master.conf
 sudo sed -i '/mailbox Trash {/a \ \ \ \ auto = create' /etc/dovecot/conf.d/15-mailboxes.conf
 
-echo "starting and enabling dovecot"
-sudo systemctl start dovecot
-sudo systemctl enable dovecot
-
-echo "*****************************************"
-echo "Tap Tap, this thing on? Checking Dovecot..."
-sudo sudo ss -lnpt | grep dovecot
-echo "*****************************************"
-
 echo "*****************************************"
 echo "Adding GoPhish and victim users..."
 sudo adduser clickme
@@ -123,5 +116,13 @@ echo "*****************************************"
 echo "*****************************************"
 sudo openssl dhparam -out /etc/dovecot/dh.pem 4096
 echo "*****************************************"
-sudo systemctl restart dovecot
+
+echo "starting and enabling dovecot"
+sudo systemctl start dovecot
+sudo systemctl enable dovecot
+
+echo "*****************************************"
+echo "Tap Tap, this thing on? Checking Dovecot..."
+sudo sudo ss -lnpt | grep dovecot
+echo "*****************************************"
 sudo systemctl restart postfix
